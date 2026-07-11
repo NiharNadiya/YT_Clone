1848012c6e97834948c998be9ca36a5effa0ce5f
Nihar1066 <niharnadia1020@gmail.com>
2026-07-11 16:58:00 +0530

    Add cloudinary and multer dependencies, implement multer middleware for file uploads, and update JSON parsing limit

--- src/app.js
+++ src/app.js
@@ -8,11 +8,12 @@ app.use(cors({
     credentials:true
 }));
 
-app.use(express.json({limit:"16kb"}));
+app.use(express.json({limit:"16kb"}));//It converts the JSON data into a JavaScript object and attaches it to the req.body property.
 
-app.use(express.urlencoded({extended:true,
+
+app.use(express.urlencoded({extended:true,//allows nested objects
     limit:"16kb"
-}))
+}))//The middleware converts the URL-encoded string into a JavaScript object.
 
 app.use(express.static("public"));
 app.use(cookieParser());
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+
+    api_key:process.env.CLOUDINARY_API_KEY,
+    
+    api_secret:process.env.CLOUDINARY_API_SECRET   
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        //upload the file on coludinary
+      const response= await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        //file upoladed
+        console.log("file uploaded successfully!",response.url);
+        return response;
+    } catch (error) {
+     fs.unlinkSync(localFilePath);//remove the locally saved temp file as file upload got failed!
+     return null;
+    }
+}   
